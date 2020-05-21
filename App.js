c0d6a8dcca11a9a4952bf8d03055c119c9cded7f
ickhov <ickhov@ucdavis.edu>
2020-05-21 14:20:34 -0700

    Added bottom tab navigation

--- App.js
+++ App.js
@@ -6,18 +6,38 @@
  */
 
 import React from 'react';
-import {
-  StatusBar
-} from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CustomStatusBar from './pages/components/statusBar';
 import HostingIsland from './pages/hosting-island/hosting-island';
+import Colors from './assets/colors';
+import Fonts from './assets/fonts';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <>
-      <CustomStatusBar/>
-      <HostingIsland/>
+      <CustomStatusBar />
+      <NavigationContainer>
+        <Tab.Navigator
+          
+          tabBarOptions={{
+            activeTintColor: Colors.white,
+            inactiveTintColor: Colors.black,
+            style: {
+              backgroundColor: Colors.primary,
+            },
+            labelStyle: {
+              fontFamily: Fonts.normal,
+              fontSize: 13,
+            }
+          }}
+        >
+          <Tab.Screen name="Marketplace" component={HostingIsland} />
+          <Tab.Screen name="Guide" component={HostingIsland} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </>
   );
 };
