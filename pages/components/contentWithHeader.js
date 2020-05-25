65fa56c7c0d3cc1e35d68eb0d7d1f3ab60676e22
ickhov <ickhov@ucdavis.edu>
2020-05-24 20:12:37 -0700

    Implement part of the calender in bug details page

---
+++ pages/components/contentWithHeader.js
@@ -0,0 +1,81 @@
+/**
+ * Custom imageButton component
+ * 
+ * Properties that can be initialize:
+ * style
+ * onPress
+ * imageSource
+ * imageStyle
+ * textStyle
+ * text
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState, useEffect, useRef } from 'react';
+
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    Animated,
+} from 'react-native';
+
+import Colors from '../../assets/colors';
+import Fonts from '../../assets/fonts';
+import RoundBorderText from './roundBorderText';
+
+export default function contentWithHeader({
+    title,
+    text
+}) {
+    return (
+        <View style={styles.container}>
+            <RoundBorderText
+                text={title}
+                containerStyle={styles.infoTitleContainer}
+                textStyle={styles.infoTitle} />
+            <RoundBorderText
+                text={text}
+                containerStyle={styles.infoTextContainer}
+                textStyle={styles.infoText} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: Colors.white,
+        width: '25%',
+    },
+    infoTitleContainer: {
+        backgroundColor: Colors.secondary,
+        borderRadius: 0,
+        width: '100%',
+    },
+    infoTitle: {
+        fontFamily: Fonts.bold,
+        fontSize: 16,
+        padding: 10,
+    },
+    infoTextContainer: {
+        backgroundColor: Colors.tertiary,
+        borderRadius: 0,
+        width: '100%',
+        borderColor: Colors.white,
+    },
+    infoText: {
+        width: '95%',
+        fontFamily: Fonts.medium,
+        fontSize: 16,
+        padding: 10,
+        color: Colors.black,
+        textAlign: 'center',
+    },
+});
--- pages/guide/museum/bugs/bugDetail.js
+++ pages/guide/museum/bugs/bugDetail.js
@@ -6,15 +6,19 @@
  */
 
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, Text, SafeAreaView, ScrollView } from 'react-native';
 import Colors from '../../../../assets/colors';
 import Fonts from '../../../../assets/fonts';
 import RoundBorderText from '../../../components/roundBorderText';
+
+import Months from '../../../helper/month';
+import ContentWithHeader from '../../../components/contentWithHeader';
 
 export default function VillagerDetailGuide({ route, navigation }) {
 
     const data = route.params.data;
     const name = lowercasetoUppercase(route.params.name);
+    const availability = data.availability;
 
     function lowercasetoUppercase(str) {
         var splitString = str.split(' ');
@@ -24,128 +28,173 @@ export default function VillagerDetailGuide({ route, navigation }) {
         return splitString.join(' ');
     };
 
+    function convertNumtoMonth(str) {
+        var splitString = str.split('-');
+        splitString[0] = Months[splitString[0]];
+        splitString[1] = Months[splitString[1]];
+        return splitString.join(' - ');
+    };
+
     return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                {/* Art Image */}
-                <Image
-                    source={{ uri: 'https://ickhov.github.io/nookeroo/images/bugs/' + data['file-name'] + '.png' }}
-                    style={styles.image} />
-                {/* Name Tab */}
-                <RoundBorderText
-                    text={name}
-                    containerStyle={styles.nameContainer}
-                    textStyle={styles.nameText} />
-            </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={styles.rootStyle}
+                contentContainerStyle={styles.rootContainer}>
+                <View style={styles.container}>
+                    <View style={styles.imageContainer}>
+                        {/* Art Image */}
+                        <Image
+                            source={{ uri: 'https://ickhov.github.io/nookeroo/images/bugs/' + data['file-name'] + '.png' }}
+                            style={styles.image} />
+                        {/* Name Tab */}
+                        <RoundBorderText
+                            text={name}
+                            containerStyle={styles.nameContainer}
+                            textStyle={styles.nameText} />
+                    </View>
 
-            {/* Buy Price Tab */}
-            <View style={styles.infoContainer}>
-                <RoundBorderText
-                    text="BUY PRICE"
-                    containerStyle={styles.infoTitleContainer}
-                    textStyle={styles.infoTitle} />
-                <RoundBorderText
-                    text={data['buy-price']}
-                    containerStyle={styles.infoTextContainer}
-                    textStyle={styles.infoText} />
-            </View>
+                    {/* Blathers' Description Tab */}
+                    <RoundBorderText
+                        text={data['museum-phrase'] + '\n' + `\t\t\t\t\t\t\t - Blathers`}
+                        containerStyle={styles.descriptionContainer}
+                        textStyle={styles.descriptionText} />
 
-            {/* Sell Price Tab */}
-            <View style={styles.infoContainer}>
-                <RoundBorderText
-                    text="SELL PRICE"
-                    containerStyle={styles.infoTitleContainer}
-                    textStyle={styles.infoTitle} />
-                <RoundBorderText
-                    text={data['sell-price']}
-                    containerStyle={styles.infoTextContainer}
-                    textStyle={styles.infoText} />
-            </View>
+                    {/* Availablity (Calender) Tab */}
+                    <View style={styles.calenderContainer}>
+                        <ContentWithHeader title={'January'} text={'N'} />
+                        <ContentWithHeader title={'Febuary'} text={'N'} />
+                        <ContentWithHeader title={'March'} text={'N'} />
+                        <ContentWithHeader title={'April'} text={'N'} />
+                        <ContentWithHeader title={'May'} text={'N'} />
+                        <ContentWithHeader title={'January'} text={'N'} />
+                        <ContentWithHeader title={'January'} text={'N'} />
+                        <ContentWithHeader title={'January'} text={'N'} />
+                    </View>
 
-            {/* Has Fake Tab */}
-            <View style={styles.infoContainer}>
-                <RoundBorderText
-                    text="Has Fake Version"
-                    containerStyle={styles.infoTitleContainer}
-                    textStyle={styles.infoTitle} />
-                <RoundBorderText
-                    text={data['hasFake'] ? 'Yes' : 'No'}
-                    containerStyle={styles.infoTextContainer}
-                    textStyle={styles.infoText} />
-            </View>
+                    {/* Information Tab */}
+                    <View style={styles.infoContainer}>
+                        <RoundBorderText
+                            text="Availability"
+                            containerStyle={styles.infoTitleContainer}
+                            textStyle={styles.infoTitle} />
+                        <RoundBorderText
+                            text={'Month (North): ' + convertNumtoMonth(availability["month-northern"])}
+                            containerStyle={styles.infoTextContainer}
+                            textStyle={styles.infoText} />
+                        <RoundBorderText
+                            text={'Month (South): ' + convertNumtoMonth(availability["month-southern"])}
+                            containerStyle={styles.infoTextContainer}
+                            textStyle={styles.infoText} />
+                        <RoundBorderText
+                            text={'Time: ' + availability.time}
+                            containerStyle={styles.infoTextContainer}
+                            textStyle={styles.infoText} />
+                    </View>
 
-        </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
+    rootStyle: {
         backgroundColor: Colors.background,
         width: '100%',
+    },
+    rootContainer: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    container: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: Colors.primary,
+        width: '90%',
+        borderRadius: 20,
+        marginVertical: 20,
     },
     imageContainer: {
         alignItems: 'center',
         flexDirection: 'row',
         justifyContent: 'flex-start',
-        width: '100%',
-        paddingTop: 20,
+        marginTop: 20,
         paddingHorizontal: 20,
-        paddingBottom: 10,
+        backgroundColor: Colors.tertiary,
+        width: '100%',
     },
     image: {
-        width: 150,
-        height: 150,
+        width: 100,
+        height: 100,
         resizeMode: 'contain',
     },
     nameContainer: {
         flex: 1,
-        width: '60%',
-        backgroundColor: Colors.primary,
-        marginLeft: 20,
-        borderRadius: 100,
+        backgroundColor: Colors.none,
+        borderRadius: 0,
     },
     nameText: {
         fontFamily: Fonts.bold,
-        fontSize: 25,
+        fontSize: 20,
+    },
+    descriptionContainer: {
+        backgroundColor: Colors.secondary,
+        borderRadius: 0,
+        width: '100%',
+    },
+    descriptionText: {
+        fontFamily: Fonts.bold,
+        fontSize: 16,
+        padding: 20,
+        textAlign: 'left',
     },
     infoContainer: {
         justifyContent: 'center',
         alignItems: 'center',
-        width: '42%',
+        width: '100%',
         marginTop: 20,
         marginHorizontal: 10,
     },
     infoTitleContainer: {
         backgroundColor: Colors.secondary,
         borderRadius: 0,
-        borderTopStartRadius: 20,
-        borderTopEndRadius: 20,
         width: '100%',
     },
     infoTitle: {
         fontFamily: Fonts.bold,
         fontSize: 18,
-        padding: 0,
-        paddingTop: 10,
-        paddingHorizontal: 10,
+        padding: 10,
     },
     infoTextContainer: {
         backgroundColor: Colors.tertiary,
         borderRadius: 0,
         width: '100%',
+        borderColor: Colors.white,
+        borderBottomWidth: 1,
     },
     infoText: {
+        width: '95%',
         fontFamily: Fonts.medium,
         fontSize: 16,
-        padding: 0,
-        paddingBottom: 10,
-        paddingHorizontal: 10,
-        color: Colors.black
+        padding: 10,
+        color: Colors.black,
+        textAlign: 'left',
+    },
+    calenderContainer: {
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: Colors.tertiary,
+        width: '100%',
+        marginTop: 20,
+    },
+    calenderBox: {
+        backgroundColor: Colors.tertiary,
+        borderRadius: 0,
+        borderColor: Colors.white,
+        borderWidth: 1,
     }
 });
